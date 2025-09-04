b9b4092bb13417c12f1b9cd4addc9fd0e1d4c2f3
Irene Lopez <irene.lopezs@protonmail.com>
2025-09-04 14:03:37 +0200

    refactor: do not use mesh references as intervention/condition names

--- src/clinical_mining/data_sources/aact/aact.py
+++ src/clinical_mining/data_sources/aact/aact.py
@@ -5,70 +5,36 @@ from clinical_mining.dataset import ClinicalStudyDataset, DrugIndicationEvidence
 
 
 def process_interventions(
-    interventions: pl.DataFrame, browse_interventions: pl.DataFrame
+    interventions: pl.DataFrame
 ) -> pl.DataFrame:
     """Extract relevant MeSH terms for interventional studies.
 
     Args:
         interventions (pl.DataFrame): The interventions or exposures (including drugs, medical devices, procedures, vaccines, and other products) of interest to the study, or associated with study arms/groups.
-        browse_interventions (pl.DataFrame): MeSH terms that describe the condition(s) being addressed by the clinical trial
     Returns:
         pl.DataFrame: Interventions table with MeSH terms.
     """
     INTERVENTION_WHITELIST = ["DRUG", "COMBINATION_PRODUCT", "BIOLOGICAL"]
-    interventions = (
+    return (
         interventions.filter(pl.col("intervention_type").is_in(INTERVENTION_WHITELIST))
         .filter(~pl.col("name").str.to_lowercase().str.starts_with("placebo"))
-        .unique()
-    )
-    browse_interventions = browse_interventions.filter(
-        pl.col("mesh_type") == "mesh-list"
-    ).select(pl.col("nct_id"), pl.col("downcase_mesh_term").alias("mesh_term"))
-
-    return (
-        interventions.join(browse_interventions, on="nct_id", how="left")
-        .with_columns(drug_name=pl.coalesce(pl.col("mesh_term"), pl.col("name")))
-        .rename({"nct_id": "studyId"})
-        .drop("name", "mesh_term")
+        .rename({"nct_id": "studyId", "name": "drug_name"})
         .unique()
     )
 
 
 def process_conditions(
-    conditions: pl.DataFrame, browse_conditions: pl.DataFrame
+    conditions: pl.DataFrame,
 ) -> pl.DataFrame:
     """Extract relevant MeSH terms for conditions.
 
     Args:
         conditions (pl.DataFrame): Conditions studied in the clinical trial.
-        browse_conditions (pl.DataFrame): MeSH terms for conditions.
 
     Returns:
         pl.DataFrame: Conditions table with MeSH terms.
     """
-    browse_conditions = (
-        browse_conditions.join(conditions.select("nct_id"), on="nct_id", how="left")
-        .filter(pl.col("mesh_type") == "mesh-list")
-        .select(pl.col("nct_id"), pl.col("downcase_mesh_term").alias("mesh_term"))
-        .unique()
-    )
-    return (
-        conditions.select(
-            pl.col("nct_id").alias("studyId"), pl.col("downcase_name")
-        )
-        .join(
-            browse_conditions.select(
-                pl.col("nct_id").alias("studyId"), pl.col("mesh_term")
-            ),
-            on="studyId",
-            how="left",
-        )
-        .with_columns(
-            disease_name=pl.coalesce(pl.col("mesh_term"), pl.col("downcase_name"))
-        )
-        .drop("downcase_name", "mesh_term")
-        .unique()
-    )
+    return conditions.rename({"nct_id": "studyId", "downcase_name": "disease_name"}).unique()
 
 
 def extract_clinical_trials(
