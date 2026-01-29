c4ed38dad0451ec4088db39902cc62b4a59f8134
Irene Lopez <irene.lopezs@protonmail.com>
2026-01-29 17:00:07 +0000

    feat(ttd): extract clinical reports

--- src/clinical_mining/data_sources/ttd.py
+++ src/clinical_mining/data_sources/ttd.py
@@ -1,13 +1,12 @@
 import polars as pl
 
-from clinical_mining.dataset import ClinicalEvidence
+from clinical_mining.dataset.clinical_report import ClinicalReport
+from clinical_mining.schemas import ClinicalReportType
 
-def extract_clinical_indication(
-    indications_path: str,
-) -> ClinicalEvidence:
-    """Extract drug/indication relationships from TTD Indications dataset."""
-    # Read the file into a list of lines
-    with open(indications_path, "r", encoding="utf-8") as file:
+
+def read_input_file(input_file_path: str) -> pl.DataFrame:
+    """Read TTD Indications dataset into a Polars DataFrame."""
+    with open(input_file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
 
     # Initialize variables
@@ -25,7 +24,7 @@ def extract_clinical_indication(
             parts = line.split("\t")
             indication = parts[1]
             icd = parts[2].replace("ICD-11: ", "").strip()
-            clinical_status = parts[3]
+            clinical_stage = parts[3]
             # Append a new row to the data list
             data.append(
                 [
@@ -33,34 +32,66 @@ def extract_clinical_indication(
                     current_drug["DRUGNAME"],
                     indication,
                     icd,
-                    clinical_status,
+                    clinical_stage,
                 ]
             )
 
-    # Create a DataFrame
-    return ClinicalEvidence(
+    return pl.DataFrame(
+        data,
+        schema=[
+            "ttd_id",
+            "drugFromSource",
+            "diseaseFromSource",
+            "icd11_id",
+            "clinical_stage",
+        ],
+        orient="row",
+    ).slice(1)  # remove first line that includes column names
+
+
+def extract_clinical_report(
+    indications_path: str,
+) -> ClinicalReport:
+    """Extract clinical reports from TTD drug/disease dataset."""
+
+    indications = read_input_file(indications_path)
+
+    reports = indications.select(
+        id=pl.concat_str(
+            [pl.col("ttd_id"), pl.lit("/"), pl.col("diseaseFromSource")]
+        ).str.to_lowercase(),
+        type=pl.lit(ClinicalReportType.CURATED_RESOURCE),
+        url=pl.concat_str(
+            [pl.lit("https://ttd.idrblab.cn/data/drug/details/"), pl.col("ttd_id")]
+        ),
+        hasExpertReview=pl.lit(True),
+        phaseFromSource=pl.col("clinical_stage").str.to_lowercase(),
+        drugFromSource=pl.col("drugFromSource").str.to_lowercase(),
+        diseaseFromSource=pl.col("diseaseFromSource").str.to_lowercase(),
+        source=pl.lit("TTD"),
+    ).unique()
+
+    mapped_reports = (
+        # TODO: call mapping function
+        reports.with_columns(
+            disease=pl.struct(
+                pl.col("diseaseFromSource"), pl.lit("CHEMBL_TO_DO").alias("diseaseId")
+            ),
+            drug=pl.struct(
+                pl.col("drugFromSource"), pl.lit("EFO_TO_DO").alias("drugId")
+            ),
+        )
+        .drop(["diseaseFromSource", "drugFromSource"])
+        .unique()
+    )
+
+    return ClinicalReport(
         df=(
-            pl.DataFrame(
-                data,
-                schema=[
-                    "ttd_id",
-                    "drugFromSource",
-                    "diseaseFromSource",
-                    "icd11_id",
-                    "clinical_status",
-                ],
-                orient="row"
+            mapped_reports.group_by(
+                [c for c in mapped_reports.columns if c not in ["disease", "drug"]]
+            ).agg(
+                pl.col("disease").unique().alias("diseases"),
+                pl.col("drug").unique().alias("drugs"),
             )
-            # remove first line that includes column names
-            .slice(1)
-            .select(
-                drugFromSource=pl.col("drugFromSource").str.to_lowercase(),
-                diseaseFromSource=pl.col("diseaseFromSource").str.to_lowercase(),
-                phase=pl.col("clinical_status").str.to_lowercase(),
-                studyId=pl.concat_str(
-                    [pl.col("ttd_id"), pl.lit("/"), pl.col("diseaseFromSource")]
-                ).str.to_lowercase(),
-                source=pl.lit("TTD"),
-            )
-        ).unique()
+        )
     )
