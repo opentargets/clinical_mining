411249676f9d8b5766bb289153d082fd6c7688a1
Irene Lopez <irene.lopezs@protonmail.com>
2026-01-30 11:39:29 +0000

    feat(ema): extract clinical reports

--- src/clinical_mining/data_sources/ema.py
+++ src/clinical_mining/data_sources/ema.py
@@ -2,32 +2,33 @@ import polars as pl
 
 from ontoma.ner.disease import extract_disease_entities
 
-from clinical_mining.dataset import ClinicalEvidence
 from clinical_mining.utils.polars_helpers import convert_polars_to_spark
 from clinical_mining.utils.spark_helpers import spark_session
+from clinical_mining.schemas import ClinicalReportType
+from clinical_mining.dataset import ClinicalReport
+from loguru import logger
 
 
-def extract_clinical_indication(
+def extract_clinical_report(
     indications_path: str,
     spark: spark_session,
-) -> ClinicalEvidence:
-    """Extract drug/indication relationships from the EMA list of human drugs."""
+) -> ClinicalReport:
+    """Extract clinical reports from the EMA list of human drugs."""
     raw = pl.read_excel(
         indications_path,
         sheet_name="Medicine",
     )
     raw.columns = raw.iter_rows().__next__()  # Assign columns names from first row
 
-    human_indications = (
-        raw.slice(1)
-        .filter(  # drop header
-            pl.col("Category") == "Human"
-        )
+    human_indications = raw.slice(1).filter(  # drop header
+        pl.col("Category") == "Human"
     )
 
     # Drop columns with all nulls to convert to spark
     non_empty_cols = [
-        series.name for series in human_indications.iter_columns() if series.null_count() < human_indications.height
+        series.name
+        for series in human_indications.iter_columns()
+        if series.null_count() < human_indications.height
     ]
     ner_extracted_indication = (
         pl.from_pandas(
@@ -50,8 +51,11 @@ def extract_clinical_indication(
         .rename({"extracted_diseases": "extracted_disease"})
     )
 
-    return ClinicalEvidence(
-        df=ner_extracted_indication.select(
+    reports = (
+        ner_extracted_indication.select(
+            id=pl.col("EMA product number").str.to_lowercase(),
+            phaseFromSource=pl.col("Medicine status").str.to_lowercase(),
+            type=pl.lit(ClinicalReportType.REGULATORY),
             drugFromSource=pl.coalesce(
                 "International non-proprietary name (INN) / common name",
                 "Active substance",
@@ -66,12 +70,42 @@ def extract_clinical_indication(
             )
             .str.to_lowercase()
             .str.split(";"),
-            phase=pl.col("Medicine status").str.to_lowercase(),
-            studyId=pl.col("EMA product number").str.to_lowercase(),
             source=pl.lit("EMA Human Drugs"),
+            url=pl.col("Medicine URL"),
+            hasExpertReview=pl.lit(False),
+            # TODO: Marketing date
         )
         .explode("drugFromSource")
         .explode("diseaseFromSource")
         # After extracting diseases, some rows may have null values (25 currently)
-        .filter(pl.col("drugFromSource").is_not_null() & pl.col("diseaseFromSource").is_not_null())
+        .filter(
+            pl.col("drugFromSource").is_not_null()
+            & pl.col("diseaseFromSource").is_not_null()
+        )
+        .unique()
+    )
+
+    mapped_reports = (
+        # TODO: call mapping function
+        reports.with_columns(
+            disease=pl.struct(
+                pl.col("diseaseFromSource"), pl.lit("CHEMBL_TO_DO").alias("diseaseId")
+            ),
+            drug=pl.struct(
+                pl.col("drugFromSource"), pl.lit("EFO_TO_DO").alias("drugId")
+            ),
+        )
+        .drop(["diseaseFromSource", "drugFromSource"])
+        .unique()
+    )
+
+    return ClinicalReport(
+        df=(
+            mapped_reports.group_by(
+                [c for c in mapped_reports.columns if c not in ["disease", "drug"]]
+            ).agg(
+                pl.col("disease").unique().alias("diseases"),
+                pl.col("drug").unique().alias("drugs"),
+            )
+        )
     )
