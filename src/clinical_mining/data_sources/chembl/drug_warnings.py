812af9439a9050fd4eeac80392c0e763eba0e303
Irene Lopez <irene.lopezs@protonmail.com>
2026-02-05 12:08:42 +0000

    feat(chembl): extract report from withdrawals and adapt schema

---
+++ src/clinical_mining/data_sources/chembl/drug_warnings.py
@@ -0,0 +1,63 @@
+"""Extraction of withdrawal clinical reports from ChEMBL drug drug warning dataset."""
+
+import polars as pl
+
+from clinical_mining.schemas import ClinicalReportType
+from clinical_mining.dataset import ClinicalReport
+
+
+def extract_clinical_report(
+    drug_warning: pl.DataFrame,
+    molecule_dictionary: pl.DataFrame,
+    warning_refs: pl.DataFrame,
+) -> ClinicalReport:
+    """Extract withdrawal clinical reports from ChEMBL drug warning dataset.
+
+    Args:
+        drug_warning: Drug warning dataset
+        molecule_dictionary: Molecule dictionary dataset
+        warning_refs: Warning references dataset
+
+    Returns:
+        ClinicalReport object with extracted reports
+    """
+
+    reports = (
+        drug_warning.join(molecule_dictionary, "molregno")
+        .join(warning_refs, "warning_id")
+        .filter(pl.col("warning_type") == "Withdrawn")
+        .select(
+            pl.concat_str(
+                # One reference can report multiple withdrawals
+                pl.col("ref_id"),
+                pl.col("chembl_id"),
+            ).hash().alias("id"),
+            pl.col("warning_type").str.to_lowercase().alias("phaseFromSource"),
+            pl.lit(ClinicalReportType.CURATED_RESOURCE).alias("type"),
+            pl.struct(
+                pl.col("efo_id").str.replace(":", "_").alias("diseaseId"),
+                pl.col("efo_term").alias("diseaseFromSource"),
+            ).alias("sideEffects"),
+            pl.struct(
+                pl.lit(None).alias("drugFromSource"),
+                pl.col("chembl_id").alias("drugId"),
+            ).alias("drug"),
+            pl.col("warning_year").alias("year"),
+            pl.col("warning_country").str.split(";").alias("countries"),
+            pl.col("ref_type").alias("source"),
+            pl.lit(True).alias("hasExpertReview"),
+            pl.col("ref_url").alias("url"),
+        )
+        .unique()
+    )
+
+    return ClinicalReport(
+        df=(
+            reports.group_by(
+                [c for c in reports.columns if c not in ["sideEffect", "drug"]]
+            ).agg(
+                pl.col("sideEffect").unique().alias("sideEffects"),
+                pl.col("drug").unique().alias("drugs"),
+            )
+        )
+    )
