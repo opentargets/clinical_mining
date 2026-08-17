a8ca9846ea648b76fc326cf5eaaa7e303f3c681e
Irene Lopez <irene.lopezs@protonmail.com>
2026-08-17 16:51:53 +0100

    chore(aact): populate  from trialStartDate

--- src/clinical_mining/data_sources/aact/clinical_report.py
+++ src/clinical_mining/data_sources/aact/clinical_report.py
@@ -184,7 +184,7 @@ def extract_clinical_report(
             hasExpertReview=pl.lit(False),
             type=pl.lit(ClinicalReportType.CLINICAL_TRIAL),
             phaseFromSource=pl.col("trial_phase"),
-            year=pl.col("trial_start_date").dt.year(),
+            year=pl.col("trial_start_date").cast(pl.Date).dt.year(),
             disease=pl.struct(
                 pl.lit(None, dtype=pl.String).alias("diseaseId"),
                 pl.col("diseaseFromSource"),
